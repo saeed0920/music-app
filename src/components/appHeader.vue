5a3acb386cdedc070027418368e64c5f449a4b1a
Saeed0920 <sd.eed1381@gmail.com>
2023-11-09 14:12:31 +0330

    feat: Add Mange.page and create component for that also fix hover Effect In Links and add Style Effect In activeRouterLink

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -3,18 +3,27 @@
   <header id="header" class="bg-[#656c73] shadow-sm">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        to="/"
+        exact-active-class="text-purple-500"
+        class="text-white font-bold uppercase text-2xl mr-4"
+        href="#"
+        >Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link to="/about" class="px-2 text-white">About</router-link>
+          </li>
           <li v-if="!userLogIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggle">Login / Register</a>
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <RouterLink to="/manage" class="px-2 text-white">Manage</RouterLink>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="logOut">Logout</a>
@@ -34,7 +43,7 @@ export default {
   name: 'appHeader',
   data() {
     return {
-      check: 'test for somehting'
+      check: 'test for something'
     }
   },
   computed: {
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,19 @@ import { createRouter, createMemoryHistory } from 'vue-router'
 import home from '@/views/home.vue'
 import error from '@/views/404.vue'
 import about from '@/views/about.vue'
+import manage from '@/views/manage.vue'
 
 const routes = [
   { path: '/', component: home },
   { path: '/404', component: error },
-  { path: '/about', component: about }
+  { path: '/about', component: about },
+  { path: '/manage', component: manage }
 ]
 
 const router = createRouter({
   history: createMemoryHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-rose-400'
 })
 
 export default router
